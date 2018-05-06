17413301bf79d49dc67ae332c630aa05e374496a
Raghava Mukkamala <rrm.digi@cbs.dk>
2018-05-06 17:55:34 +0200

    updates

---
+++ 00_python_version.py
@@ -0,0 +1,7 @@
+# To check the version of the Python, type the following commands
+# in the windows console (windows) or terminal (mac)
+# first type 'python' to enter into python mode.
+
+import  sys
+
+print (sys.version)
